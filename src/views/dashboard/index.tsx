965f69d7bb42335da07c62fd468b7927d13cf36a
aeither <36173828+aeither@users.noreply.github.com>
2022-11-15 20:23:36 +0100

    add search collection

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { RoyaltyByMonth } from "components/charts/RoyaltyByMonth";
 import { RoyaltyByUser } from "components/charts/RoyaltyByUser";
 import { TotalByMarketplace } from "components/charts/TotalByMarketplace";
-import { FC } from "react";
+import { collections } from "data/collections";
+import { FC, useState } from "react";
+import SelectSearch from "react-select-search";
 
 const dataByMonth = [
   {
@@ -44,7 +46,38 @@ const dataByMP = [
   { name: "OpenSea", royalty: 189 },
 ];
 
-export const DashboardView: FC = ({}) => {
+function renderCollection(props, option, _, className) {
+  const imgStyle = {
+    borderRadius: "50%",
+    verticalAlign: "middle",
+    marginRight: 10,
+  };
+
+  return (
+    <div className="flex flex-row">
+      <button
+        {...props}
+        className={
+          "flex items-center w-full text-white hover:bg-purple-700 p-2 text-sm"
+        }
+        type="button"
+      >
+        <img
+          alt=""
+          style={imgStyle}
+          width="28"
+          height="28"
+          src={option.image}
+        />
+        <span>{option.name}</span>
+      </button>
+    </div>
+  );
+}
+
+export const DashboardView: FC = () => {
+  const [collection, setCollection] = useState("s");
+
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
@@ -54,6 +87,25 @@ export const DashboardView: FC = ({}) => {
         <h2 className="font-bold text-lg">
           Royalties accrued in the last 6 months
         </h2>
+
+        <div className="flex flex-row">
+          <SelectSearch
+            placeholder="Select a collection"
+            options={collections}
+            search
+            autoComplete="on"
+            renderOption={renderCollection}
+            onChange={(value) => setCollection(value.toString())}
+            value={collection}
+          />
+          <button
+            className="btn btn-primary"
+            onClick={() => console.log(collection)}
+          >
+            Search
+          </button>
+        </div>
+
         <RoyaltyByMonth data={dataByMonth} />
         <h2 className="font-bold text-lg">Top payers</h2>
         <RoyaltyByUser data={dataByUser} />
