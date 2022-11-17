c096b72ffc2fe343ff6602996e3a303524ba7511
Yan <yan.alan.he@gmail.com>
2022-11-17 13:51:09 -0500

    Added the API and query to get sales and their associated royalties fees

---
+++ src/pages/api/mintactivities/[...slug].ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from "axios";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {   
+   const { slug } = req.query;
+   if (!slug || slug.length < 2) {
+      res.status(400).json({ message: 'both path parameters update_authority and collection_symbol must be provided' });      
+   }
+
+   let updateauthority = slug[0] as string;
+   let collectionsymbol = slug[1] as string;      
+   console.log(`updateauthority=${updateauthority}`);
+   console.log(`collectionsymbol=${collectionsymbol}`);
+
+   var apikey = '0dec5037-f67d-4da8-9eb6-97e2a09ffe9a';
+   var url = `${process.env.CORAL_CUBE_API_BASE}${apikey}/inspector/getMintActivities?update_authority=${updateauthority}&collection_symbol=${collectionsymbol}`;
+   console.log(`url=${url}`)
+   
+   var config = {
+      method: 'get',
+      url: url,      
+      headers: { 'Accept': 'application/json' }
+   };  
+
+   try {
+      const results = await axios(config);
+      res.status(200).json(results.data);
+   } catch (error) {
+      res.status(500).json(error);
+   }
+}
+
+export default handler
+
--- src/queries/queries.tsx
+++ src/queries/queries.tsx
@@ -9,3 +9,8 @@ export const getCreator = async (mint_address: string) => {
    const response = await fetch("/api/creator/" + mint_address);
    return response.json();   
 }
+
+export const getMintActivities = async (updateauthority: string, collectionsymbol: string) => {   
+   const response = await fetch("/api/mintactivities/" + updateauthority + "/" +collectionsymbol);
+   return response.json();
+}
