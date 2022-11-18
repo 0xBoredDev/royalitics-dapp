d470d4be372be36340a736196b57fe5fcb8fbffb
aeither <36173828+aeither@users.noreply.github.com>
2022-11-18 10:56:04 +0100

    add chartjs

--- src/views/analytics/index.tsx
+++ src/views/analytics/index.tsx
@@ -7,6 +7,77 @@ import { collections } from "data/collections";
 import { Collection } from "models/collection";
 import { FC, useState } from "react";
 import SelectSearch from "react-select-search";
+import { Pie, Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+} from "chart.js";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title
+);
+
+const pieData = {
+  labels: [
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp1",
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp2",
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp3",
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp4",
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp5",
+    "8kQayFPyKcJCdJYs8FTSTP3yNsTeKcAewegewTY7UVp6",
+  ],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+        "rgb(21, 201, 25)",
+        "rgb(174, 21, 201)",
+        "rgb(201, 135, 21)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const lineData = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+      order: 2,
+    },
+    {
+      label: "My Second Dataset",
+      data: [23, 84, 12, 9, 12, 34, 23],
+      fill: false,
+      borderColor: "rgb(255, 205, 25)",
+      tension: 0.2,
+      order: 1,
+    },
+  ],
+};
 
 const imgStyle = {
   borderRadius: "50%",
@@ -98,7 +169,7 @@ export const AnalyticsView: FC = () => {
 
   return (
     <div className="md:container mx-auto p-4">
-      <div className="flex flex-row justify-center pb-6">
+      <div className="flex flex-row justify-center">
         <SelectSearch
           placeholder="Select a collection"
           options={collections}
@@ -114,21 +185,37 @@ export const AnalyticsView: FC = () => {
           value={collection.name}
         />
       </div>
-      <div className="flex flex-row">
-        <div className="basis-1/4">
-          <img
-            alt=""
-            style={imgStyle}
-            width="120"
-            height="120"
-            src={collection.image}
-          />
+      <section className="py-12">
+        <div className="flex flex-row">
+          <div className="basis-1/4">
+            <img
+              alt=""
+              style={imgStyle}
+              width="120"
+              height="120"
+              src={collection.image}
+            />
+          </div>
+          <div className="basis-3/4 p-2">
+            <div className="text-4xl font-bold pb-5">{collection.name}</div>
+            <div className="">{collection.description}</div>
+          </div>
         </div>
-        <div className="basis-3/4 p-2">
-          <div className="text-4xl font-bold pb-5">{collection.name}</div>
-          <div className="">{collection.description}</div>
+      </section>
+      <section className="py-12">
+        <div className="grid grid-cols-5 gap-4">
+          <div className="col-span-3">
+            <div className="p-8 ">
+              <Line data={lineData} />
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="p-8 ">
+              <Pie data={pieData} />
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
       {/* <div className="md:hero-content flex flex-col">
         <RoyaltyByMonth data={dataByMonth} />
         <h2 className="font-bold text-lg">Top payers</h2>
