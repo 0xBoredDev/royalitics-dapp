1cca9dd0a738eec437d2a5b6591adccf5a8cf672
Yan <yan.alan.he@gmail.com>
2022-11-23 11:06:36 -0500

    Ensured correct filter applied for the API in getting all sales

--- src/pages/api/mintactivities/[...slug].ts
+++ src/pages/api/mintactivities/[...slug].ts
@@ -7,7 +7,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
    if (!slug || slug.length < 2) {
       res.status(400).json({ message: 'both path parameters update_authority and collection_symbol must be provided' });      
    }
-
    
    let updateauthority = slug[0] as string;
    let collectionsymbol = slug[1] as string;      
@@ -17,12 +16,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       days = slug[2] as string;      
    }
 
-   //console.log(`updateauthority=${updateauthority}`);
-   //console.log(`collectionsymbol=${collectionsymbol}`);
-
+   //console.log(`days=${days}`);  
    var apikey = '0dec5037-f67d-4da8-9eb6-97e2a09ffe9a';
    var url = `${process.env.CORAL_CUBE_API_BASE}${apikey}/inspector/getMintActivities?update_authority=${updateauthority}&collection_symbol=${collectionsymbol}`;
-   console.log(`url=${url}`)
+   //console.log(`url=${url}`)
    
    var config = {
       method: 'get',
@@ -34,13 +31,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       let results = await axios(config);
       var daysnumber: number = +days;
       let date_daysago = moment().utc().add(-daysnumber, 'days').format(); 
-      console.log(`date_daysago=${date_daysago}`)
+      //console.log(`date_daysago=${date_daysago}`)
 
       let data = results.data.filter(item => {
          //console.log(`item.time=${item.time}`)
          let daysdiff = moment(item.time).diff(moment(date_daysago), 'days');
-         //console.log(`daysfiff=${daysfiff}`)
-         return daysdiff <= daysnumber
+         //console.log(`daysdiff=${daysdiff}`)
+         //console.log('-------')
+         return daysdiff > 0
       });
 
       res.status(200).json(data);
