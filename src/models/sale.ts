75f0a7400bfd73ede279d2bb4dadce4a9b1e7af0
caughmanconsulting <caughmanconsulting@gmail.com>
2022-12-10 10:56:16 -0500

    model update

--- src/models/sale.ts
+++ src/models/sale.ts
@@ -1,4 +1,14 @@
 export class Sale {
+  day: string;
+  month: string;
+  year: string;
+  timestamp: string;
+  royaltiesCollected: number;
+  royaltiesUnCollected: number;
+  marketplace: string;
+  paidFullRoyalty: boolean;
+  paidHalfRoyalty: boolean;
+  sellerFee: number;
   constructor(
     day: string,
     month: string,
@@ -16,6 +26,7 @@ export class Sale {
     year = year;
     timestamp = timestamp;
     royaltiesCollected = royaltiesCollected;
+    royaltiesUnCollected = royaltiesUnCollected;
     marketplace = marketplace;
     paidFullRoyalty = paidFullRoyalty;
     paidHalfRoyalty = paidHalfRoyalty;
